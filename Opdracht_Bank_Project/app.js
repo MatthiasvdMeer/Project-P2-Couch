12ffe29ff3f29dafaa166ca416866ce0e4d9f21b
MatthiasvdMeer <9026846@student.tcrmbo.nl>
2024-11-26 13:51:18 +0100

    Versie 2 tussensave

--- Opdracht_Bank_Project/app.js
+++ Opdracht_Bank_Project/app.js
@@ -1,3 +1,17 @@
+function login(){
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    if(username == "AdminUser" && password == "Admin123"){
+        alert("Welcome in the Couch!");
+        window.location.href = ("page1.html")
+}
+    else{
+        alert("Please try again.");
+        
+}
+}
+
 function showCurrentDate() {
     const date = new Date();
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
